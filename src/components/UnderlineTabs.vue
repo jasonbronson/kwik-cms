1ceb0ce81ab666cfbb8724c8b7d95c427e9c3bd6
kasunChan <kasun.chan9476@gmail.com>
2022-06-17 21:34:24 +0530

    Underline component

---
+++ src/components/UnderlineTabs.vue
@@ -0,0 +1,215 @@
+<template>
+  <div class="items-center justify-between flex text-sm px-4">
+    <div class="flex items-center">
+      <div class="bg-white px-4 border-solid border-2 rounded mr-2">
+        <ul
+          class="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400"
+        >
+          <li
+            class="mr-2"
+            v-for="(tabData, index) in data"
+            :key="index"
+            @click="changeTab(tabData)"
+          >
+            <a
+              href="#"
+              class="inline-flex items-center gap-2 p-4 rounded-t-lg border-b-2 border-transparent group"
+              :class="[
+                {
+                  'text-blue-600 border-blue-600 active dark:text-blue-500 dark:border-blue-500 group':
+                    isCurrentTab(tabData),
+                },
+                {
+                  'hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300':
+                    !isCurrentTab(tabData),
+                },
+              ]"
+            >
+              <span class="text-base"><i :class="tabData.icon"></i></span>
+              <span>{{ tabData.title }}</span>
+            </a>
+          </li>
+          <!--          <li class="mr-2">
+            <a
+              href="#"
+              class="inline-flex p-4 text-blue-600 rounded-t-lg border-b-2 border-blue-600 active dark:text-blue-500 dark:border-blue-500 group"
+              aria-current="page"
+            >
+              <svg
+                class="mr-2 w-5 h-5 text-blue-600 dark:text-blue-500"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
+                ></path>
+              </svg>
+              Draft
+            </a>
+          </li>
+          <li class="mr-2">
+            <a
+              href="#"
+              class="inline-flex p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group"
+            >
+              <svg
+                class="mr-2 w-5 h-5 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-gray-300"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z"
+                ></path>
+              </svg>
+              Published
+            </a>
+          </li>
+          <li class="mr-2">
+            <a
+              href="#"
+              class="inline-flex p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group"
+            >
+              <svg
+                class="mr-2 w-5 h-5 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-gray-300"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path>
+                <path
+                  fill-rule="evenodd"
+                  d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              Scheduled
+            </a>
+          </li>-->
+        </ul>
+      </div>
+      <div class="mr-2">
+        <div class="dropdown inline-block relative">
+          <button
+            class="bg-white text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center"
+          >
+            <span class="mr-1">Author</span>
+            <svg
+              class="fill-current h-4 w-8"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path
+                d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+              />
+            </svg>
+          </button>
+          <ul class="dropdown-menu absolute hidden text-gray-700 pt-2">
+            <li class="">
+              <a
+                class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
+                href="#"
+                >One</a
+              >
+            </li>
+            <li class="">
+              <a
+                class="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
+                href="#"
+                >Two</a
+              >
+            </li>
+            <li class="">
+              <a
+                class="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
+                href="#"
+                >Three is the magic number</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <div class="dropdown inline-block relative">
+          <button
+            class="bg-white text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center"
+          >
+            <span class="mr-1">Categories</span>
+            <svg
+              class="fill-current h-4 w-8"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path
+                d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+              />
+            </svg>
+          </button>
+          <ul class="dropdown-menu absolute hidden text-gray-700 pt-2">
+            <li class="">
+              <a
+                class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
+                href="#"
+                >One</a
+              >
+            </li>
+            <li class="">
+              <a
+                class="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
+                href="#"
+                >Two</a
+              >
+            </li>
+            <li class="">
+              <a
+                class="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
+                href="#"
+                >Three is the magic number</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="w-1/6 flex justify-end tex-primary-500">Export All Posts</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UnderlineTab",
+  props: {
+    data: {
+      type: Array,
+      default: () => [],
+    },
+    value: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  mounted() {
+    if (
+      this.data.length > 0 &&
+      !this.data.find((tab) => tab.title == this.value.title)
+    ) {
+      this.changeTab(this.data[0]);
+    }
+  },
+  methods: {
+    isCurrentTab(tabData) {
+      return tabData.title == this.value.title;
+    },
+    changeTab(tabData) {
+      this.$emit("input", tabData);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.dropdown:hover .dropdown-menu {
+  display: block;
+}
+</style>
--- src/views/Blogs.vue
+++ src/views/Blogs.vue
@@ -29,178 +29,69 @@
             New Post
           </button>
         </div>
-        <div class="items-center justify-between flex text-sm px-4">
-          <div class="flex items-center">
-            <div class="bg-white px-4 border-solid border-2 rounded mr-2">
-              <ul
-                class="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400"
+        <div>
+          <UnderlineTabs
+            :data="BlogTabs"
+            v-model="currentBlogTab"
+            @input="handleEventTypeChange"
+          >
+            <template v-slot:all>
+              <svg
+                class="mr-2 w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <li class="mr-2">
-                  <a
-                    href="#"
-                    class="inline-flex p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group"
-                  >
-                    <svg
-                      class="mr-2 w-5 h-5 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-gray-300"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
-                        clip-rule="evenodd"
-                      ></path>
-                    </svg>
-                    All
-                  </a>
-                </li>
-                <li class="mr-2">
-                  <a
-                    href="#"
-                    class="inline-flex p-4 text-blue-600 rounded-t-lg border-b-2 border-blue-600 active dark:text-blue-500 dark:border-blue-500 group"
-                    aria-current="page"
-                  >
-                    <svg
-                      class="mr-2 w-5 h-5 text-blue-600 dark:text-blue-500"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
-                      ></path>
-                    </svg>
-                    Draft
-                  </a>
-                </li>
-                <li class="mr-2">
-                  <a
-                    href="#"
-                    class="inline-flex p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group"
-                  >
-                    <svg
-                      class="mr-2 w-5 h-5 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-gray-300"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z"
-                      ></path>
-                    </svg>
-                    Published
-                  </a>
-                </li>
-                <li class="mr-2">
-                  <a
-                    href="#"
-                    class="inline-flex p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group"
-                  >
-                    <svg
-                      class="mr-2 w-5 h-5 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-gray-300"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path>
-                      <path
-                        fill-rule="evenodd"
-                        d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z"
-                        clip-rule="evenodd"
-                      ></path>
-                    </svg>
-                    Scheduled
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div class="mr-2">
-              <div class="dropdown inline-block relative">
-                <button
-                  class="bg-white text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center"
-                >
-                  <span class="mr-1">Author</span>
-                  <svg
-                    class="fill-current h-4 w-8"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
-                    />
-                  </svg>
-                </button>
-                <ul class="dropdown-menu absolute hidden text-gray-700 pt-2">
-                  <li class="">
-                    <a
-                      class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
-                      href="#"
-                      >One</a
-                    >
-                  </li>
-                  <li class="">
-                    <a
-                      class="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
-                      href="#"
-                      >Two</a
-                    >
-                  </li>
-                  <li class="">
-                    <a
-                      class="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
-                      href="#"
-                      >Three is the magic number</a
-                    >
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div>
-              <div class="dropdown inline-block relative">
-                <button
-                  class="bg-white text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center"
-                >
-                  <span class="mr-1">Categories</span>
-                  <svg
-                    class="fill-current h-4 w-8"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
-                    />
-                  </svg>
-                </button>
-                <ul class="dropdown-menu absolute hidden text-gray-700 pt-2">
-                  <li class="">
-                    <a
-                      class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
-                      href="#"
-                      >One</a
-                    >
-                  </li>
-                  <li class="">
-                    <a
-                      class="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
-                      href="#"
-                      >Two</a
-                    >
-                  </li>
-                  <li class="">
-                    <a
-                      class="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
-                      href="#"
-                      >Three is the magic number</a
-                    >
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="w-1/6 flex justify-end tex-primary-500">
-            Export All Posts
-          </div>
+                <path
+                  fill-rule="evenodd"
+                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </template>
+            <template v-slot:draft>
+              <svg
+                class="mr-2 w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </template>
+            <template v-slot:published>
+              <svg
+                class="mr-2 w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </template>
+            <template v-slot:scheduled>
+              <svg
+                class="mr-2 w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </template>
+          </UnderlineTabs>
         </div>
         <!-- options selected -->
         <div class="px-4">
@@ -249,7 +140,43 @@
   </div>
 </template>
 <script>
-export default {};
+import UnderlineTabs from "@/components/UnderlineTabs";
+export default {
+  components: { UnderlineTabs },
+  data() {
+    return {
+      BlogTabs: [
+        {
+          title: "All",
+          id: "all",
+          icon: "fas fa-user-circle",
+        },
+        {
+          title: "Draft",
+          id: "draft",
+          icon: "fas fa-th-large",
+        },
+        {
+          title: "Published",
+          id: "published",
+          icon: "fas fa-sliders-v",
+        },
+        {
+          title: "Scheduled",
+          id: "scheduled",
+          icon: "fas fa-clipboard-list",
+        },
+      ],
+      currentBlogTab: {},
+      password: "",
+    };
+  },
+  methods: {
+    handleEventTypeChange(selectedValue) {
+      this.currentBlogTab = selectedValue;
+    },
+  },
+};
 </script>
 <style lang="scss" scoped>
 @import url("https://fonts.googleapis.com/css?family=Roboto:300&display=swap");
