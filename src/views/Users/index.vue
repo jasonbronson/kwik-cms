92f9439eb988f457a6d1722d82d43d7894f099ae
jbronsongxm <jbronson@gxm.com>
2022-06-21 14:57:32 +0000

    update users to flexbox

--- src/views/Users/index.vue
+++ src/views/Users/index.vue
@@ -1,52 +1,58 @@
 <template>
   <div class="bg-primary-600 min-h-screen">
-    <are-you-sure v-if="deleteUserConfirm" text="Do you want to delete this user?" @yes="deleteUser" @no="deleteUserConfirm = false"></are-you-sure>
+    <are-you-sure
+      v-if="deleteUserConfirm"
+      text="Do you want to delete this user?"
+      @yes="deleteUser"
+      @no="deleteUserConfirm = false"
+    ></are-you-sure>
     <div class="wrapper">
-      <div class="title">
-        <span>Manage User</span>
+      <div class="flex justify-between p-10">
+        <span class="text-primary-400">Manage User</span>
         <button class="button button-dark" @click="addUser">Add User</button>
       </div>
-      <div class="table-wrapper bg-white">
-        <table class="table table-auto border-collapse border border-slate-300">
-          <thead>
-            <tr class="table-header bg-indigo-400 text-white">
-              <td class="p-2">User</td>
-              <td class="p-2">Role</td>
-              <td class="p-2">Email</td>
-              <td class="p-2">Actions</td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr class="even:bg-amber-100 odd:bg-blue-100" v-for="(item, idx) in listData" :key="idx">
-              <td class="p-2">{{ item.first_name + " " + item.last_name }}</td>
-              <td class="p-2">{{ item.role.name }}</td>
-              <td class="p-2">{{ item.email }}</td>
-              <td class="p-2">
-                <button class="button button-light" @click="editUser(item)">Edit</button>
-                <button class="button button-light" @click="showPopup(item)">Delete</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+      <div class="">
+        <div class="flex justify-between text-white px-4 p-1 bg-primary-400">
+          <div class="p-2">User</div>
+          <div class="p-2">Role</div>
+          <div class="p-2">Email</div>
+          <div class="p-2">Actions</div>
+        </div>
+          <div
+            class="flex justify-between items-center px-4 p-1 text-primary-400 even:bg-amber-100 odd:bg-blue-100"
+            v-for="(item, idx) in listData"
+            :key="idx"
+          >
+            <div class="p-2">{{ item.first_name + " " + item.last_name }}</div>
+            <div class="p-2">{{ item.role.name }}</div>
+            <div class="p-2">{{ item.email }}</div>
+            <div class="p-2">
+              <button class="button button-dark mr-5" @click="editUser(item)">
+                Edit
+              </button>
+              <button class="button button-dark" @click="showPopup(item)">
+                Delete
+              </button>
+            </div>
+        </div>
       </div>
     </div>
   </div>
-
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import AreYouSure from '@/components/global/AreYouSure.vue'
+import { mapState } from "vuex";
+import AreYouSure from "@/components/global/AreYouSure.vue";
 export default {
   name: "Users",
   data() {
     return {
       deleteUserConfirm: false,
-      selectedUser: {}
-    }
+      selectedUser: {},
+    };
   },
   components: {
-    AreYouSure
+    AreYouSure,
   },
   computed: {
     ...mapState({
@@ -58,32 +64,42 @@ export default {
   },
   methods: {
     addUser() {
-      this.$router.push("/users/new")
+      this.$router.push("/users/new");
     },
     editUser(item) {
-      this.$router.push(`/users/edit/${item.id}`)
+      this.$router.push(`/users/edit/${item.id}`);
     },
     showPopup(item) {
       this.deleteUserConfirm = true;
-      this.selectedUser = item
+      this.selectedUser = item;
     },
     async deleteUser() {
       try {
-        this.loading = true
+        this.loading = true;
         await this.$store.dispatch("users/deleteUser", this.selectedUser);
-        this.deleteUserConfirm = false
+        this.deleteUserConfirm = false;
         await this.$store.dispatch("users/fetchAndSetUsers");
-        this.$router.push('/users')
+        this.$router.push("/users");
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style scoped lang="scss">
-.wrapper{
+div {
+  box-sizing: border-box;
+}
+.table {
+  display: flex;
+}
+.flex-table {
+  display: flex;
+  flex-flow: row wrap;
+}
+.wrapper {
   padding: 0 20px;
 }
 .button {
@@ -101,27 +117,5 @@ export default {
   border: 1px solid black;
   margin-right: 10px;
 }
-.title {
-  display: flex;
-  justify-content: space-between;
-  padding: 20px 0px;
-  span {
-    font-size: 16px;
-    font-weight: 700;
-    color: #44456f;
-  }
-}
-.table-wrapper {
-  width: 100%;
-  .table {
-    width: 100%;
-    tr:nth-child(even) {
-      background-color: #5a6386;
-      color: white;
-    }
-  }
-}
-.table-header {
-  background-color: #121a3e;
-}
+
 </style>
